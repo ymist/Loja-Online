177d0bc73e7190529f22d9d3a8661b1ba096bec3
Bruno Rufatto Oliveira Coleraus <118026486+ymist@users.noreply.github.com>
2024-03-04 21:12:31 -0300

    Atualizar tabelas do banco de dados

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/users/CreateUserController.js";
 import { DetailUserController } from "./controllers/users/DetailUserController.js";
 import { AuthUserController } from "./controllers/users/AuthUserController.js";
+import { UpdateUserController } from "./controllers/users/UpdateUserController.js";
 
 const routes = Router();
 
@@ -12,6 +13,7 @@ const routes = Router();
 routes.post("/signup", new CreateUserController().handle)
 routes.get("/perfil", new DetailUserController().handle)
 routes.post("/login", new AuthUserController().handle)
+routes.put("/user/:id",  new UpdateUserController().handle)
 
 
 export {routes}
