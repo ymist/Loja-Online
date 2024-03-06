6b5868805d3f2bf372dee4a10749f72970ae82db
ymist <brunorufattocoleraus@gmail.com>
2024-03-06 19:26:10 -0300

    comment

--- backend/src/prisma/index.js
+++ backend/src/prisma/index.js
@@ -1,5 +1,5 @@
-import {PrismaClient} from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
+// prisma to database
+const prismaClient = new PrismaClient();
 
-const prismaClient = new PrismaClient()
-
-export default prismaClient
+export default prismaClient;
