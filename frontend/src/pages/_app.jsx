0851af282d9ddb5220f15b1350bcae4ca86bc139
ymist <brunorufattocoleraus@gmail.com>
2024-04-07 14:56:48 -0300

    app

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -2,20 +2,17 @@ import "../styles/globals.scss";
 import "../components/Swipper/styles.css";
 import { ThemeProvider } from "@emotion/react";
 import { NextUIProvider } from "@nextui-org/react";
-import { AuthProvider } from "@/providers/auth_provider";
 
 import { newTheme } from "@/utils/theme";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<AuthProvider>
-				<NextUIProvider>
-					<ThemeProvider theme={newTheme}>
-						<Component {...pageProps} />
-					</ThemeProvider>
-				</NextUIProvider>
-			</AuthProvider>
+			<NextUIProvider>
+				<ThemeProvider theme={newTheme}>
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</NextUIProvider>
 		</>
 	);
 }
