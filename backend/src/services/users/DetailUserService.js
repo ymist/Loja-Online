ce6c8bc363ae1aecb75a57f229726850466bed81
Bruno Rufatto Oliveira Coleraus <brunorufattocoleraus@gmail.com>
2024-04-09 12:27:28 -0300

    DetailUserService

--- backend/src/services/users/DetailUserService.js
+++ backend/src/services/users/DetailUserService.js
@@ -1,18 +1,21 @@
 import prismaClient from "../../prisma/index.js";
 
-class DetailUserService{
-    async execute(user_id){
-        const user = await prismaClient.user.findFirst({
-            where:{
-                id: user_id
-            },
-            select:{
-                id: true,
-                name:true, 
-                email:true
-            },
-        })
-        return {user}
-    }
+class DetailUserService {
+	async execute(user_id) {
+		const user = await prismaClient.user.findFirst({
+			where: {
+				id: user_id,
+			},
+			select: {
+				id: true,
+				name: true,
+				email: true,
+				orders: true,
+				address: true,
+				cart: true,
+			},
+		});
+		return { user };
+	}
 }
-export {DetailUserService}
+export { DetailUserService };
