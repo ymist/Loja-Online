33e3dbe7396c3fdc311a477e0de69dc79c31de3a
Bruno Rufatto Oliveira Coleraus <brunorufattocoleraus@gmail.com>
2024-04-12 08:45:49 -0300

    Atualizar DetailUserController

--- backend/src/controllers/users/DetailUserController.js
+++ backend/src/controllers/users/DetailUserController.js
@@ -2,11 +2,11 @@ import { DetailUserService } from "../../services/users/DetailUserService.js";
 
 class DetailUserController {
 	async handle(req, res) {
-		const { id } = req.body;
+		const { user_id } = req.body;
 
 		const detailUserService = new DetailUserService();
-		console.log(id);
-		const user = await detailUserService.execute(id);
+		console.log(user_id);
+		const user = await detailUserService.execute(user_id);
 
 		return res.json(user.user);
 	}
