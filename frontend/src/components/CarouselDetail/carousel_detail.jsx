8b57ce572cb87e18a53302c61997cb2bfc4409af
ymist <brunorufattocoleraus@gmail.com>
2024-04-15 20:54:22 -0300

    Pagina de Detalhes

---
+++ frontend/src/components/CarouselDetail/carousel_detail.jsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import { useState } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+import { useMediaQuery } from "@mui/material";
+
+export default function CarouselDetail({ images }) {
+	const isDesktop = useMediaQuery("(min-width: 900px)");
+
+	// Estado para controlar o slide atual
+	const [currentSlide, setCurrentSlide] = useState(0);
+
+	// Função para avançar para o próximo slide
+	const nextSlide = () => {
+		setCurrentSlide((prevSlide) =>
+			prevSlide === images.length - 1 ? 0 : prevSlide + 1,
+		);
+	};
+
+	// Função para retroceder para o slide anterior
+	const previousSlide = () => {
+		setCurrentSlide((prevSlide) =>
+			prevSlide === 0 ? images.length - 1 : prevSlide - 1,
+		);
+	};
+
+	return (
+		<>
+			<Carousel className="w-full h-full">
+				<CarouselContent>
+					{images?.map((image, index) => (
+						<CarouselItem key={index} className="pl-0">
+							<div className="w-full border-none">
+								<Card className="h-full border-none">
+									<img
+										className="object-cover w-full h-full border-none"
+										src={`/tmp_products/${image}`}
+									/>
+								</Card>
+							</div>
+						</CarouselItem>
+					))}
+				</CarouselContent>
+				{/* Paginação com bolinhas de rádio */}
+
+				{/* Botões de navegação */}
+				{isDesktop && (
+					<>
+						<CarouselPrevious />
+						<CarouselNext />
+					</>
+				)}
+			</Carousel>
+		</>
+	);
+}
--- frontend/src/pages/product/[id].jsx
+++ frontend/src/pages/product/[id].jsx
@@ -3,22 +3,25 @@ import { apiClient } from "@/services/apiClient";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { Rating } from "@mui/material";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import CarouselDetail from "@/components/CarouselDetail/carousel_detail";
 
 export default function DetailProduct() {
 	const router = useRouter();
 	const { id } = router.query;
 	const [product, setProduct] = useState(null);
-	console.log(product);
+	const [value, setValue] = useState(2);
 
 	useEffect(() => {
 		const fetchProduct = async () => {
 			try {
 				const response = await apiClient(`/product/${id}`);
 
-				console.log(response);
 				if (response.data?.error) {
 					return <div>pagina nao encontrada</div>;
 				}
+				console.log(response.data);
 				setProduct(response.data);
 			} catch (error) {
 				console.log(error);
@@ -30,19 +33,58 @@ export default function DetailProduct() {
 	return (
 		<>
 			<Header />
-			<main className="min-h-screen w-screen p-5 flex flex-row md:flex-col">
-				<div className="w-2/4">
-					{product?.banner.map((item, i) => {
+			<main className="w-screen flex  my-8 flex-col gap-6 lg:flex-row lg:px-[10%] lg:py-[2%]">
+				<div className="w-[500px] h-[500px] p-16 flex flex-col items-center border-1 border-palette-base-gray500/50">
+					{/*product?.banner.map((item, i) => {
 						return (
-							<Image
+							<img
 								src={`/tmp_products/${item}`}
-								width={180}
-								height={200}
+								className="w-full h-full"
 							/>
 						);
-					})}
+					})*/}
+					<CarouselDetail
+						images={[
+							product?.banner[0],
+							product?.banner[0],
+							product?.banner[0],
+						]}
+					/>
 				</div>
-				<div className="w-2/4"></div>
+				<div className="w-2/4">
+					<div className="card-body px-0 py-0 h-full flex justify-between flex-col">
+						<h2 className="card-title font-medium max-h-20 text-[28px] tracking-8  px-6 py-2">
+							{product?.name}
+						</h2>
+						<div className="card-actions justify-start pl-6 pt-2">
+							<div className="badge badge-warning p-4 text-[16px]">
+								{product?.brand.name}
+							</div>
+							<div className="badge badge-warning p-4 text-[16px]">
+								{product?.category.name}
+							</div>
+						</div>
+						<Rating
+							name="simple-controlled"
+							size="large"
+							sx={{
+								padding: " 0px 1em",
+							}}
+							value={value}
+							onChange={(event, newValue) => {
+								setValue(newValue);
+							}}
+						/>
+						<h3 className="text-neutral-800 font-bold flex justify-end pr-8 text-[28px]">
+							R$ {product?.price}
+						</h3>
+						<div className="flex items-end justify-center gap-1">
+							<button className="btn btn-square border-transparent bottom-6 bg-palette-primary-light text-palette-base-main w-8/12">
+								<AddShoppingCartIcon />
+							</button>
+						</div>
+					</div>
+				</div>
 			</main>
 		</>
 	);
