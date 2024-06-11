54e43d3b5a5b2bb1e73a56076869f2fa33a2fa3d
ymist <brunorufattocoleraus@gmail.com>
2024-06-11 18:44:34 -0300

    Ajustar autocomplete

--- frontend/src/components/ui/custom_autocomplete.jsx
+++ frontend/src/components/ui/custom_autocomplete.jsx
@@ -13,28 +13,20 @@ export default function CustomAutocomplete({ products }) {
 			placeholder="O que você procura?"
 			labelPlacement="inside"
 			className="max-w-full m-0 ">
-			{(product) => {
+			{(product, index) => {
 				return (
-					<AutocompleteItem
-						key={product.id}
-						textValue={product.name}
-						onClick={() => router.push(`/product/${product.id}`)}>
+					<AutocompleteItem key={product.id} textValue={product.name} onClick={() => router.push(`/product/${product.id}`)}>
 						<div className="flex gap-6 items-center h-32  ">
 							<Image
-								src={"/tmp_products/" + product.banner[0]}
+								// src={"/tmp_products/" + product.banner[0]}
+								src={"/produto-de-beleza-e-cosmeticos-com-tons-rosados-suaves.png"}
 								width={100}
 								height={100}
 							/>
 							<div className="flex flex-col">
-								<span className="text-small">
-									{product.name}
-								</span>
-								<span className="text-tiny text-default-400">
-									{product.brand.name}
-								</span>
-								<span className="text-tiny text-default-400">
-									{product.category.name}
-								</span>
+								<span className="text-small">{"Produto " + product?.id.split("-")[2].toUpperCase()}</span>
+								<span className="text-tiny text-default-400">{"Marca" + index + 1}</span>
+								<span className="text-tiny text-default-400">{"Categoria" + index + 1}</span>
 							</div>
 						</div>
 					</AutocompleteItem>
